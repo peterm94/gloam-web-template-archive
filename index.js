f70001615bccd796397c83527950bf48a3555f51
Peter Mandile <peterm1994@hotmail.com>
2022-05-28 20:07:45 +1000

    linked to gloam-engine repo

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-import * as wasm from "hello-wasm-pack";
+import * as wasm from "gloam-engine";
 
 wasm.greet();
+
+let game = wasm.Game.new("gello");
+
+let e = game.add_entity();
+
+console.log(e);
+
+game.add_component_web(e, (1, 2));
