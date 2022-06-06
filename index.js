0b1203fdefd60e1a376913d40f281f476e2dd205
Peter Mandile <peterm1994@hotmail.com>
2022-06-06 19:19:13 +1000

    added mutability test

--- index.js
+++ index.js
@@ -11,3 +11,12 @@ console.log(e);
 game.add_component_web(e, {"how": "idk"});
 game.add_component_web(e, 4);
 
+// Test mutability.
+let ret = game.get_component_web(e, "Number");
+console.log(ret)
+ret = 144;
+
+let ret2 = game.get_component_web(e, "Number");
+console.log(ret2)
+
+game.start();
